80faa92e905f55117eb83b14afb35e379f31de7c
Romir Kulshrestha <romir.kulshrestha@gmail.com>
2021-09-12 14:46:54 +0200

    music api

--- bass.ino
+++ bass.ino
@@ -1,7 +1,8 @@
 #include "music_api.h"
 
 Bass bass(4,5,6,7);
-int sequence[] = {0, 1, 2, 3};
+Player player(bass);
+char song[] = "AB;A;B;DB;DB;";
 const int rest = 500;
 long long noteTime = 0;
 int head = 0;
@@ -14,15 +15,7 @@ void setup()
 
 void loop()
 {   
-    size_t numOfNotes = sizeof(sequence) / sizeof(sequence[0]);
-    long long t = millis();
     
-    //Serial.println(head);
-    
-    if(t - noteTime >= rest) {
-      noteTime = t;
-      head = (head + 1) % numOfNotes;
-      bass.pluck(sequence[head], 150);
-    }
-    bass.tick(t);
+    player.load(song);
+    player.tick();
 }
--- music_api.h
+++ music_api.h
@@ -1,10 +1,60 @@
 #include "Arduino.h"
 #include "bass_api.h"
 
-#ifndef Bass_h
-#define Bass_h
+#ifndef Music_h
+#define Music_h
 
-void play(sequence) {
+class Player
+{
+    Bass bass;
+    int tempo = 90;
+    int frequency = 90 * 1000 / 60;
+    char *song;
+    bool playing = false;
+    long long noteTime = 0;
 
-}
+public:
+    Player(Bass b) : bass(b)
+    {
+    }
+
+    int **load(char *s)
+    {
+        song = s;
+        playing = true;
+    }
+
+    void playNext()
+    {
+        do
+        {
+            char n = *song++;
+            if(n == ';') break;
+            switch (n)
+            {
+            case 'E':
+            case 'e':
+                bass.pluck(0, 150);
+                break;
+            case 'A':
+            case 'a':
+                bass.pluck(1, 150);
+                break;
+            case 'D':
+            case 'd':
+                bass.pluck(1, 150);
+                break;
+            case 'B':
+            case 'b':
+                bass.pluck(1, 150);
+                break;
+            }
+        } while(*song);
+    }
+
+    void tick() {
+        if(millis() - noteTime >= frequency) playNext();
+        bass.tick();
+    }
+};
 #endif
